a3106790cc76ca0c5f399e6cb6c840be91e11297
Justyna KUJAWA <justy.kujawa@gmail.com>
2021-11-09 20:54:38 +0100

    changeLETCONST

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
-var argButtonName, buttonRock, buttonPaper;
+{
+    let argButtonName, buttonRock, buttonPaper;
  //COMPUTER MOVE
-function getMoveName(argMoveId) {
+const getMoveName = function (argMoveId) {
     console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
     if (argMoveId == 1) {
     return 'kamień';
@@ -14,7 +15,7 @@ function getMoveName(argMoveId) {
     }
 }
 //RESULT
-function displayResult(argPlayerMove, argComputerMove) {
+const displayResult = function (argPlayerMove, argComputerMove) {
     console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
     if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
     printMessage('Wygrywasz!');
@@ -29,18 +30,18 @@ function displayResult(argPlayerMove, argComputerMove) {
     }
     printMessage('Zagrałem ' + argComputerMove + 'Ty zagrales ' + argPlayerMove);
 }
-function buttonClicked(argButtonName) {
+const buttonClicked = function (argButtonName) {
     clearMessages();
     console.log(argButtonName + ' został kliknięty');
     
-    var playerMove, computerMove, randomNumber, argMoveId, argPlayerMove, argComputerMove
+    // let argMoveId, argPlayerMove, argComputerMove
     
     //GRACZ MOVE
 
     console.log('ruch gracza to: ' + argButtonName);
-    randomNumber = Math.floor(Math.random() * 3 + 1);
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('wylosowana liczba to: ' + randomNumber);
-    computerMove = getMoveName(randomNumber);
+    let computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
     displayResult(argButtonName, computerMove);
 
@@ -52,3 +53,4 @@ buttonPaper = document.getElementById('button-paper');
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
 buttonScissors = document.getElementById('button-scissors');
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
+}
